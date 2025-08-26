959abf4c6749fa8b6abbef727684188fd2011d08
GorodeN <rshamaluk@gmail.com>
2025-08-26 17:56:47 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const bracketsMap = new Map(bracketsConfig);
+
+  for (let i = 0; i < str.length; i += 1) {
+    const currentSymbol = str[i];
+    const lastElement = stack[stack.length - 1];
+
+    if (bracketsMap.has(currentSymbol)) {
+      if (
+        currentSymbol === bracketsMap.get(currentSymbol) &&
+        lastElement === currentSymbol
+      ) {
+        stack.pop();
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else if (
+      !stack.length ||
+      bracketsMap.get(stack.pop()) !== currentSymbol
+    ) {
+      return false;
+    }
+  }
+
+  return !stack.length;
 };
